8036ccb4f82a509ca31fb7b16ba0df7d266ad3d8
lshannon <luke.shannon@gmail.com>
2016-10-22 13:16:19 -0400

    Added DB Connection Details

--- cf-flyway-db/src/main/java/com/lukeshannon/CfFlywayDbApplication.java
+++ cf-flyway-db/src/main/java/com/lukeshannon/CfFlywayDbApplication.java
@@ -1,16 +1,10 @@
 	package com.lukeshannon;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CfFlywayDbApplication {
-	
-	@Autowired
-	private DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CfFlywayDbApplication.class, args);
