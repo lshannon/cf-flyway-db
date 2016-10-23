0a4434a7180bf6253e1f37a0263bab8805e97545
lshannon <luke.shannon@gmail.com>
2016-10-23 13:30:11 -0400

    First check In

---
+++ cf-simple-data/src/main/java/com/lukeshannon/CfSimpleDataApplication.java
@@ -0,0 +1,12 @@
+package com.lukeshannon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfSimpleDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfSimpleDataApplication.class, args);
+	}
+}
---
+++ cf-simple-data/src/main/java/com/lukeshannon/model/Customer.java
@@ -0,0 +1,38 @@
+package com.lukeshannon.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customer")
+public class Customer {
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private Integer id;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+}
